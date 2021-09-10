81e6913ee7d7773aa3391aee3a22cb7686d5214a
Nathan Uphoff <nathan.uphoff@mediamonks.com>
2021-09-10 11:09:11 +0200

    Fix isSerializable circular reference

--- src/lib/predicates/isSerializable.ts
+++ src/lib/predicates/isSerializable.ts
@@ -1,11 +1,7 @@
 import { Predicate, Serializable } from '../types'
-import { or } from '../generics/or'
 import { isSerializableArray } from './isSerializableArray'
 import { isSerializableObject } from './isSerializableObject'
 import { isSerializablePrimitive } from './isSerializablePrimitive'
 
-export const isSerializable: Predicate<Serializable> = or(
-  isSerializableArray,
-  isSerializableObject,
-  isSerializablePrimitive,
-)
+export const isSerializable: Predicate<Serializable> = (value: unknown): value is Serializable =>
+  isSerializablePrimitive(value) || isSerializableObject(value) || isSerializableArray(value)
