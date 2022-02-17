e9ccc3e59bc4a233636fcb30656dd78c7a74c0da
Leroy Korterink <5376407+leroykorterink@users.noreply.github.com>
2022-02-17 11:38:28 +0100

    Update isSome.ts

--- src/lib/predicates/isSome.ts
+++ src/lib/predicates/isSome.ts
@@ -2,6 +2,9 @@ import { Predicate, Some } from '../types'
 
 /**
  * Checks if a value is a value other then `null`, `undefined`, or not NaN.
+ *
+ * Using `value === value` for the NaN check instead of `Number.isNaN(value) === false` 
+ * because it's slightly faster (~1%).
  */
 export const isSome: Predicate<Some> = (value: unknown): value is Some =>
-  value != null && Number.isNaN(value) === false
+  value != null && value === value
