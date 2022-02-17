ebc8f6d458e56316d5d8abc9621bbceac26675b4
Leroy Korterink <5376407+leroykorterink@users.noreply.github.com>
2022-02-17 11:22:54 +0100

    Add more explicit isNaN case
    It wasn't clear from the JSDoc and code that the `value === value` condition is used to check for not `NaN` values

--- src/lib/predicates/isSome.ts
+++ src/lib/predicates/isSome.ts
@@ -1,7 +1,7 @@
 import { Predicate, Some } from '../types'
 
 /**
- * Checks if a value is a value other then `null` or `undefined`.
+ * Checks if a value is a value other then `null`, `undefined`, or not NaN.
  */
 export const isSome: Predicate<Some> = (value: unknown): value is Some =>
-  value != null && value === value
+  value != null && Number.isNaN(value) === false
